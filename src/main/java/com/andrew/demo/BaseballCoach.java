0b73deccff7420bfa0b371c3f5db000918a89537
AndreyBelonog <belonogandrey@gmail.com>
2019-04-30 15:42:28 +0300

    Initial commit

---
+++ src/main/java/com/andrew/demo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.andrew.demo;
+
+public class BaseballCoach {
+
+    public String getDailyWorkout() {
+        return "Spent 30 minutes on exercises";
+    }
+}
---
+++ src/main/java/com/andrew/demo/MyApp.java
@@ -0,0 +1,13 @@
+package com.andrew.demo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+//        create object
+        BaseballCoach baseballCoach = new BaseballCoach();
+
+//        use object
+        System.out.println(baseballCoach.getDailyWorkout());
+    }
+}
